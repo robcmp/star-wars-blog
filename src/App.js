5475427dec599efa718fb6716a75cc4fad69d461
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-08-30 22:43:21 -0400

    added Layout component as main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Layout from './js/components/Layout';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Layout />
   );
 }
 
