e1355348fac7481c8df16b2afff3069367407e30
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-08-30 23:03:07 -0400

    Card componente added

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -1,1 +1,18 @@
 import React from "react";
+
+const Card = (props) => {
+    return (
+        <div className="card">
+            <img src={props.imagen} className="card-img-top" alt="" />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <div className="card-footer text-center">
+                <button className="btn btn-primary"> Find Out More!</button>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
