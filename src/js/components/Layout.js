f3e799e0ed94ec2e827b16aaba483afbdfb18120
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-08-31 22:15:39 -0400

    added container to Layout.js

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,15 +3,19 @@ import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Home from "../views/Home";
 import Navbar from "./Navbar";
 
+
 const Layout = () => {
     return <Router>
         <Navbar />
-        <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
-            <Route render={() => <h1>Not found</h1>}></Route>
-        </Switch>
+        <div className="container">
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route render={() => <h1>Not found</h1>}></Route>
+            </Switch>
+        </div>
+
     </Router>
 }
 export default Layout;
