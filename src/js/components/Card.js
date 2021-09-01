823b3158ea1903d9dc022b807b2aa6756167b5fe
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-09-01 16:46:00 -0400

    added more descriptions to the Card with props usages

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -7,6 +7,8 @@ const Card = (props) => {
             <div className="card-body">
                 <h5 className="card-title">{props.data.name}</h5>
                 <p className="card-text">{(/people/g).test(props.data.url) === true ? props.data.gender : props.data.population}</p>
+                <p className="card-text">{(/people/g).test(props.data.url) === true ? props.data.hair_color : props.data.terrain}</p>
+                <p className="card-text">{(/people/g).test(props.data.url) === true ? props.data.eye_color : ""}</p>
             </div>
             <div className="card-footer text-center">
                 <button className="btn btn-primary"> Find Out More!</button>
