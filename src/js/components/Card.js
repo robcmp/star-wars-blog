45ec126253964d55c013ec5ba3804f2ff21df151
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-09-01 00:22:08 -0400

    added ternary to handle data in component Card.js need to be fixed, changed props name for src img tag in Card.js, changed tabindex to tabIndex in component Navbar.js

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -3,10 +3,10 @@ import React from "react";
 const Card = (props) => {
     return (
         <div className="card">
-            <img src={props.imagen} className="card-img-top" alt="" />
+            <img src={props.image} className="card-img-top" alt="" />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 className="card-title">{props.data.name}</h5>
+                <p className="card-text">{/*props === 'peoples' ? props.data.gender : props.data.population*/}</p>
             </div>
             <div className="card-footer text-center">
                 <button className="btn btn-primary"> Find Out More!</button>
