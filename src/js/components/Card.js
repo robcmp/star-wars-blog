da46b0fd3e90f92443c4dccdfcbccce7aedbac60
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-09-01 16:32:18 -0400

    component Card.js changed with descriptions with ternary

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -6,7 +6,7 @@ const Card = (props) => {
             <img src={props.image} className="card-img-top" alt="" />
             <div className="card-body">
                 <h5 className="card-title">{props.data.name}</h5>
-                <p className="card-text">{/*props === 'peoples' ? props.data.gender : props.data.population*/}</p>
+                <p className="card-text">{(/people/g).test(props.data.url) === true ? props.data.gender : props.data.population}</p>
             </div>
             <div className="card-footer text-center">
                 <button className="btn btn-primary"> Find Out More!</button>
