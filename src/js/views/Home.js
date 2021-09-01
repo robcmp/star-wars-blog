13f5b249685503dd14200e046066d12466e52c5e
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-08-31 22:14:11 -0400

    Home with hooks added

--- src/js/views/Home.js
+++ src/js/views/Home.js
@@ -1,8 +1,25 @@
 import React from "react";
+import '../css/Home.css';
+import { useState, useEffect } from "react";
+import Card from "../components/Card";
 
 const Home = () => {
+    const [planetas, setPlanetas] = useState([]);
+
+    useEffect(() => {
+        fetch("https://swapi.dev/api/planets/", {
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
+        }).then(response => response.json())
+            .then(data => setPlanetas(data.results))
+    }, [])
     return (
-        <h1>Home</h1>
+        <>
+            <h1>Planets</h1>
+            <div className="scrolling-wrapper">
+                {planetas.map((planeta, i) => <Card data={planeta} />)}
+            </div>
+        </>
     );
 }
 export default Home;
