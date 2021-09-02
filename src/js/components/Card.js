88420908b86a9d771643a27407a1318cc7ba852c
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-09-01 22:02:04 -0400

    added heart button, ternary conditions updated

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -1,17 +1,23 @@
 import React from "react";
 
 const Card = (props) => {
+
     return (
         <div className="card">
             <img src={props.image} className="card-img-top" alt="" />
             <div className="card-body">
                 <h5 className="card-title">{props.data.name}</h5>
-                <p className="card-text">{(/people/g).test(props.data.url) === true ? props.data.gender : props.data.population}</p>
-                <p className="card-text">{(/people/g).test(props.data.url) === true ? props.data.hair_color : props.data.terrain}</p>
-                <p className="card-text">{(/people/g).test(props.data.url) === true ? props.data.eye_color : ""}</p>
+                <p className="card-text">{(/people/g).test(props.data.url) === true ? <span>Gender: {props.data.gender}</span> : ((/planets/g).test(props.data.url) === true ? <span>Population: {props.data.population}</span> : "")}</p>
+                <p className="card-text">{(/people/g).test(props.data.url) === true ? <span>Hair-color: {props.data.hair_color}</span> : ((/planets/g).test(props.data.url) === true ? <span>Terrain: {props.data.terrain}</span> : <span>Model: {props.data.model}</span>)}</p>
+                <p className="card-text">{(/people/g).test(props.data.url) === true ? <span>Eyes-color: {props.data.eye_color}</span> : ""}</p>
             </div>
-            <div className="card-footer text-center">
-                <button className="btn btn-primary"> Find Out More!</button>
+            <div className="card-footer text-center row">
+                <div class="col-md-8">
+                    <button className="btn btn-primary"> Find Out More!</button>
+                </div>
+                <div class="col-md-4">
+                    <button className="btn btn-outline-warning"><i class="bi bi-heart"></i></button>
+                </div>
             </div>
         </div>
     );
