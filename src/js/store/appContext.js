c2e2f20d4b1cf1911f3730ecf78654ad7261737a
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-09-03 20:04:26 -0400

    getState added to useState in appContext.js

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from "react";
+import { getState } from "./flux";
 
 export const Context = React.createContext(null);
 
 const injectContext = PassedComponent => {
     const StoreWrapper = (props) => {
-        const [state, setState] = useState();
+        const [state, setState] = useState(
+            getState({
+                getActions: () => state.actions,
+                getStore: () => state.store,
+                setStore: updatedStore => setState({
+                    store: Object.assign(state.store, updatedStore),
+                    actions: { ...state.actions }
+                })
+            })
+        );
 
         return (
             <Context.Provider value={state}>
