32891b0fd598565a65415a36f6ca0499f04cb2ed
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-09-06 20:05:05 -0300

    added view DetailsVehic.js

---
+++ src/js/views/DetailsVehic.js
@@ -0,0 +1,23 @@
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+const DetailsVehic = (props) => {
+    let { store, actions } = useContext(Context);
+    const params = useParams();
+
+    useEffect(() => {
+        actions.getDetailChar(params.id);
+    }, [])
+
+    return (
+        <div className="p-4 p-md-5 mb-4 text-white rounded bg-dark">
+            <div className="col-md-6 px-0">
+                <h1 className="fst-italic">Hi </h1>
+                <p className="lead my-3">Likn this post’s contents.</p>
+                <p className="lead mb-0"><a href="#" className="text-white fw-bold">Continue reading.</a></p>
+            </div>
+        </div>
+    )
+}
+export default DetailsVehic;
