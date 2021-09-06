10a9cbbe92759adb4980e7eafb18a71bcab73708
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-09-06 09:58:51 -0300

    modified params for context variables

--- src/js/views/Details.js
+++ src/js/views/Details.js
@@ -13,12 +13,13 @@ const Details = (props) => {
     useEffect(() => {
         actions.getDetail(urlFinal);
     }, [])
+
     return (
         <div className="p-4 p-md-5 mb-4 text-white rounded bg-dark">
             <div className="col-md-6 px-0">
                 <h1 className="fst-italic">Hi </h1>
-                <p className="lead my-3">Likn this post’s contents.{store.details}</p>
-                <p className="lead mb-0"><a href="#" className="text-white fw-bold">Continue reading...</a></p>
+                <p className="lead my-3">Likn this post’s contents.{console.log(store.details)}</p>
+                <p className="lead mb-0"><a href="#" className="text-white fw-bold">Continue reading.</a></p>
             </div>
         </div>
     )
