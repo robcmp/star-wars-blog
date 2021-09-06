8f1d1ddc6dcf9c7e9bb4302af2ce626841dbe9e2
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-09-05 22:55:21 -0300

    added variables to handle url object and useEffect hook

--- src/js/views/Details.js
+++ src/js/views/Details.js
@@ -1,15 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 const Details = (props) => {
-    const { store, actions } = useContext(Context);
+    let { store, actions } = useContext(Context);
     const params = useParams();
+    let urlFinal = "";
+    urlFinal = params.url;
+    urlFinal = urlFinal.replace(/\_/g, "/");
+    store = urlFinal;
+
+    useEffect(() => {
+        actions.getDetail(urlFinal);
+    }, [])
     return (
         <div className="p-4 p-md-5 mb-4 text-white rounded bg-dark">
             <div className="col-md-6 px-0">
-                <h1 className="fst-italic">Hi ${console.log(String(params.url))}</h1>
-                <p className="lead my-3">Likn this post’s contents.${String(params.url)}</p>
+                <h1 className="fst-italic">Hi </h1>
+                <p className="lead my-3">Likn this post’s contents.{store.details}</p>
                 <p className="lead mb-0"><a href="#" className="text-white fw-bold">Continue reading...</a></p>
             </div>
         </div>
