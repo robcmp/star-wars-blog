dd11c6b9901ea0434093fc02c65e5b20b37e186f
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-09-05 21:45:20 -0300

    added propertys in objects store and actions to handle events in app

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,8 +1,29 @@
 export const getState = ({ setStore, getStore, getActions }) => {
 
     return {
-        store: {},
-        actions: {}
+        store: {
+            favoriteList: [],
+            details: []
+        },
+        actions: {
+            addFavorite: (ev) => {
+                const store = getStore();
+                setStore([...store.favoriteList, store]);
+                console.log(ev.target);
+                store.map((li, key) => <li className="dropdown-item" key={key} >{li}<span className="ps-5"><button onClick={() => {
+                    // deleteTask(key);
+                }}><i class="bi bi-x"></i></button></span></li>)
+            },
+            getDetail: (ev) => {
+                const store = getStore();
+                fetch(store, {
+                    method: "GET",
+                    headers: { "Content-Type": "application/json" }
+                }).then(response => response.json())
+                    .then(data => setStore({ details: data.results }))
+
+            }
+        }
     };
 
 }
