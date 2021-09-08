f5c859bbff8f061202ec6f919893418d6b6c8798
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-09-08 16:30:01 -0300

    added grid system bootstrap, added more character info sections

--- src/js/views/DetailsChar.js
+++ src/js/views/DetailsChar.js
@@ -13,12 +13,34 @@ const DetailsChars = (props) => {
     return (
         <div className="p-5 mb-4 bg-light rounded-3 bg-dark mt-5">
             <div className="container-fluid py-5">
-                <h1 className="display-5 fw-bold text-light">{store.details.name}</h1>
-                <p className="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-                
+                <div className="row">
+                    <div className="col-md-6">
+                        <img src="https://via.placeholder.com/800x600" className="img-fluid" alt="" />
+                    </div>
+                    <div className="col-md-6">
+                        <h1 className="display-5 fw-bold text-light">{store.details.name}</h1>
+                        <p className="col-md-8 fs-4 text-light">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+                    </div>
+                </div>
+                <div className='row text-light mt-3'>
+                    <div className="col-md-3">
+                        {store.details.height}
+                    </div>
+                    <div className="col-md-3">
+                        {store.details.mass}
+                    </div>
+                    <div className="col-md-3">
+                        {store.details.hair_color}
+                    </div>
+                    <div className="col-md-3">
+                        {store.details.eye_color}
+                    </div>
+                </div>
+
+
             </div>
         </div>
-       
+
     )
 }
 export default DetailsChars;
