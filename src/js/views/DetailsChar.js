5f5cbc1ebfc6be2ded989c507ba57d4360c0304f
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-09-09 09:30:51 -0300

    modified useEffect to test deployment

--- src/js/views/DetailsChar.js
+++ src/js/views/DetailsChar.js
@@ -11,7 +11,7 @@ const DetailsChars = (props) => {
     }
     useEffect(() => {
         fetchDetails();
-    }, [])
+    }, [fetchDetails])
 
     return (
         <div className="p-5 mb-4 bg-light rounded-3 bg-dark mt-5">
