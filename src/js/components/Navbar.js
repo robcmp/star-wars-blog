b4daf66e594a9ccb34389c1408851b7411dd0194
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-09-13 15:55:24 -0300

    modified navbar brand logo

--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = (props) => {
 
     return <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <div className="container-fluid">
-            <Link className="navbar-brand ms-5" to="/"><img src={'img/sw.png'} alt="..." height="45"/></Link>
+            <Link className="navbar-brand ms-5" to="/"><img src={'img/sw_logo.png'} alt="..." height="45"/></Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
